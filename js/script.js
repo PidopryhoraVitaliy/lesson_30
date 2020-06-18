7c2db3bf7aea73906c3d0da1b69fc5484a64e745
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2020-06-18 17:19:30 +0300

    lesson 30

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан 2",
+        "Лига справедливости 2",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+document.querySelectorAll('.promo__adv').forEach(
+	elem => elem.remove()
+);
+document.querySelectorAll('.promo__genre').forEach(
+	elem => elem.textContent = 'Драма'
+);
+document.querySelectorAll('.promo__bg').forEach(
+	elem => elem.style.backgroundImage = 'url(./img/bg.jpg)'
+);
+
+movieDB.movies.sort();
+movieDB.movies.forEach(
+	(item, index) => console.log(index+1, item)
+);
+
+const list = document.getElementsByClassName('promo__interactive-list')[0];
+
+
+
+
+
+
+
